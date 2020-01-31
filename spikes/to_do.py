7d0a61e677d13b1446c87817687919939f96743b
ohsa <litarrow@gmail.com>
2020-01-30 20:02:21 -0800

    add delete buttons, create show_tasks function
    also reformat data structure to list of dictionaries

--- spikes/to_do.py
+++ spikes/to_do.py
@@ -15,21 +15,43 @@
 import tkinter as tk
 from tkinter import *
 from tkinter.ttk import *
+from functools import partial
 
 #function to add a task
 #currently adds a task to the dictionary of tasks and prints. Also adds to the listbox and displays
 def add_task():
 	# get the inputs
-	sample_list[input_task_name.get()] = input_task_priority.get()
+	sample_list.append({'name': input_task_name.get(), 'priority': input_task_priority.get()})
 	task_box.insert(END, input_task_name.get() + '\t' + '\t' + input_task_priority.get() + '\n')
+	task_box.window_create(task_box.index("end"), window = tk.Button(task_box, text="Remove", command = partial(rem_task, sample_list, len(sample_list) - 1)))
 	# reset input boxes/selections
 	input_task_name.delete(0, 'end')
 	input_task_priority.current(0)
 
+	show_tasks()
+
 	print(sample_list)
 
-def rem_task():
-    return
+
+def rem_task(i):
+	sample_list.pop(i)
+	show_tasks()
+
+	print(sample_list)
+
+
+	
+def show_tasks():
+	task_box.delete("1.0", "end")
+	#insert a title into the taskbox
+	task_box.insert(END, 'Task' + '\t' + '\t' + 'Priority' + '\n')
+
+	#insert each task into the taskbox
+	for i in range(len(sample_list)):
+		task_box.insert(END, sample_list[i]['name'] + '\t' + '\t' + sample_list[i]['priority'] + '\n')
+		task_box.window_create(task_box.index("end"), window = tk.Button(task_box, text="Remove", command = partial(rem_task, i)))
+		task_box.insert(END, "\n")
+
 
 #name of window
 root = tk.Tk()
@@ -40,10 +62,10 @@ h = '400'
 root.geometry('{}x{}'.format(w, h))
 
 #generate sample list as a starting point
-sample_list = {}
-sample_list["Feed Cat"] = "High"
-sample_list["Mow Lawn"] = "Low"
-sample_list["Go to DMV"] = "Low"
+sample_list = []
+sample_list.append({'name': "Feed Cat", 'priority': "High"})
+sample_list.append({'name': "Mow Lawn", 'priority': "Low"})
+sample_list.append({'name': "Go to DMV", 'priority': "Low"})
 
 #create window label
 lbl_title = tk.Label(root, text="To-Do List", bg="white")
@@ -52,13 +74,6 @@ lbl_title.grid(row=0,column=0, sticky=W)
 #create box to display tasks
 task_box = Text(root,width = 25,height=10)
 task_box.grid(row=2,column=0)
-
-#insert a title into the taskbox
-task_box.insert(END, 'Task' + '\t' + '\t' + 'Priority' + '\n')
-
-#insert each task into the taskbox
-for i in sample_list:
-	task_box.insert(END, i + '\t' + '\t' + sample_list[i] + '\n')
 
 #create labels for text entry fields
 tk.Label(root, text="New Task Name:", bg="white").grid(row=7, sticky=W)
@@ -76,6 +91,8 @@ input_task_priority.current(0)
 #create button to add new task
 add_new_button = Button(root, text="Add New Task", command=add_task)
 add_new_button.grid(row=11, sticky=W)
+
+show_tasks()
 
 if __name__ == "__main__":
 	root.mainloop()
