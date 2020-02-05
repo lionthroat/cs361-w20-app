fd83445d6243143fcf38c03d8b1a1ec8a561426c
Lauren Work <workla@oregonstate.edu>
2020-02-04 21:48:41 -0800

    Skeleton - broken up into files
    Breaking up the classes into separate files, creating a main document that imports all other documents, added comments to easily communicate between teams

---
+++ First Iteration code/taskList.py
@@ -0,0 +1,48 @@
+##########################################################################
+#   Class: TaskList
+#   Summary:  This class holds the Task List.  It has a list of task class
+#       instances and functions to add and remove tasks.  It can also save
+#       tasks to a JSON file or load from a JSON file.
+##########################################################################
+
+import json
+import tasks.py
+
+class TaskList:
+#########################################################################
+#   Variables used in this class:
+#       __listOfTasks: A list of Task class instances for each task
+#           to be listed in the to do list
+#########################################################################
+##########################################################################
+#   Initializing method
+##########################################################################
+    def __init__(self):
+        self.__listOfTasks = []
+    def addTaskToList(self, title, priority):
+        ##add/remove team to add here
+    def removeTaskFromList(self, title, priority):
+        ##add/remove team to add here, no clue what parameters you want
+##########################################################################
+#   Function: loadJSON
+#   Takes: file name to load from
+#   Returns: nothing
+#   Summary: This function takes a JSON file and loads the data into the
+#       __listOfTasks list by creating instances of task loaded with the
+#       next entry's data and placing that instance in the list.
+##########################################################################
+    def loadJSON(self, filename):
+        with open(filename) as jsonHandle:
+            jsonData = json.load(jsonHandle)
+            print(jsonData)
+        # placeholder: will add remaining functionality Thursday 2/4
+##########################################################################
+#   Function: saveToJSON
+#   Takes: file name to save to
+#   Returns: nothing
+#   Summary: This function takes the current __listOfTasks and converts it
+#       and places into a JSON file.
+##########################################################################
+    def saveToJSON(self, fileName):
+        # placeholder: will add remaining functionality Thursday 2/4
+        
---
+++ First Iteration code/toDoMain.py
@@ -0,0 +1,15 @@
+##########################################################################
+#   Program: To Do List
+#   Summary:  This to do list allows a user to keep track of their to do
+#       items.  It can allow the user to add or remove items, and set the
+#       item's title and priority status.
+##########################################################################
+
+import taskList.py
+##need to import whatever file is created for viewability here
+
+
+def main():
+    ####add here for whatever should be called from all the other files
+
+main()
