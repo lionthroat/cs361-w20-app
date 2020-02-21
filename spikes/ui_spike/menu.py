8b9712b3f584cb96463e46bdeeaa8fce44ee5ae0
Lauren Work <workla@oregonstate.edu>
2020-02-20 17:10:00 -0800

    Update of menu
    Export placeholders, quit shortkey, help about section

--- spikes/ui_spike/menu.py
+++ spikes/ui_spike/menu.py
@@ -10,8 +10,13 @@ class AppMenuBar(tk.Menu):
         # File Menu
         fileMenu = tk.Menu(self, tearoff=False)
         self.add_cascade(label="File",underline=0, menu=fileMenu)
-        fileMenu.add_command(label="Exit", underline=1, command=self.quit)
+        fileMenu.add_command(label="Export To Do List", underline=1, command=self.exportToDo)
+        fileMenu.add_command(label="Export Appointments", underline=1, command=self.exportAppts)
+        fileMenu.add_separator()
+        fileMenu.add_command(label="Exit", underline=1, command=self.quit, accelerator="Ctrl+Q")
 
+        self.bind_all("<Control-q>", self.quit)
+        
         # Theme Menu (roll into settings menu eventually?)
         themeMenu = tk.Menu(self, tearoff=False)
         self.add_cascade(label="Theme",underline=0, menu=themeMenu)
@@ -22,7 +27,19 @@ class AppMenuBar(tk.Menu):
         ## set theme 2
         themeMenu.add_command(label="Mint Chocolate Chip", underline=1, command=partial(self.setMintTheme, parent, themes))
 
-    def quit(self):
+
+        # Help Menu
+        helpMenu = tk.Menu(self, tearoff=False)
+        self.add_cascade(label="Help",underline=0, menu=helpMenu)
+        helpMenu.add_command(label="About", underline=1, command=self.aboutUs)
+        
+    def exportToDo(self):
+        sys.exit(0)
+
+    def exportAppts(self):
+        sys.exit(0)
+        
+    def quit(self, event=None):
         sys.exit(0)
 
     def setDraculaTheme(self, parent, themes):
@@ -32,3 +49,6 @@ class AppMenuBar(tk.Menu):
     def setMintTheme(self, parent, themes):
         parent.config(background='#FFFFFF')
         themes.theme_use('mintChocolateChip')
+        
+    def aboutUs(self):
+        sys.exit(0)
